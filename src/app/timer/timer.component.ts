82eec9f38f9800a0a0a62d5665808da50ca1af5d
wendko <wendko.88@gmail.com>
2019-07-07 08:00:36 +0800

    fix timer issue

--- src/app/timer/timer.component.ts
+++ src/app/timer/timer.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FoodService } from '../food.service';
-import { tap, finalize } from 'rxjs/operators';
+import { tap, finalize, takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-timer',
@@ -16,8 +16,12 @@ export class TimerComponent implements OnInit {
   ngOnInit() {
     this.gameDuration = this.foodService.gameDurationInSeconds;
     this.foodService.timeKeeper(true)
-      .pipe(tap(() => this.gameDuration--),
-        finalize(() => this.gameDuration = -1))
+      .pipe(
+        tap(() => this.gameDuration--),
+        takeUntil(this.foodService.timeRunsOut()),
+        takeUntil(this.foodService.gameWon$),
+        finalize(() => this.gameDuration = -1)
+      )
       .subscribe();
   }
 
