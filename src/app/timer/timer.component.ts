94af967d9c34033c4b2233b1f64a87fe02d0ac68
wendko <wendko.88@gmail.com>
2019-10-16 21:09:07 +0800

    refactor: timer and sidebar

--- src/app/timer/timer.component.ts
+++ src/app/timer/timer.component.ts
@@ -1,36 +1,19 @@
-import { Component, OnInit } from '@angular/core';
-import { FoodService } from '../food.service';
-import { tap, finalize, takeUntil, map } from 'rxjs/operators';
-import { Time } from '@angular/common';
-import { TimeService } from '../time.service';
+import { Component } from '@angular/core';
 import { Subject } from 'rxjs';
+import { finalize, map } from 'rxjs/operators';
+import { TimeService } from '../time.service';
 
 @Component({
     selector: 'app-timer',
     templateUrl: './timer.component.html',
     styleUrls: ['./timer.component.scss']
 })
-export class TimerComponent implements OnInit {
-    gameDuration: number;
-
-    constructor(
-        private timeService: TimeService,
-        private foodService: FoodService
-    ) { }
-
-
-    timeIsUp$ = new Subject<string>();
-
-
-    showTimeIsUp: boolean;
-    showTimesUpMsg$ = this.timeIsUp$.pipe(tap(_ => this.showTimeIsUp = true));
+export class TimerComponent {
+    constructor(private timeService: TimeService) { }
+    timeIsUp$ = new Subject<Boolean>();
+    showTimesUpMsg$ = this.timeIsUp$.asObservable();
     time$ = this.timeService.gameTimer$.pipe(
-        map(val => (this.timeService.gameEnds / 1000) - val), // reverse shown
-        finalize(() => this.timeIsUp$.next())
+        map(val => (this.timeService.gameEnds / 1000) - val),
+        finalize(() => this.timeIsUp$.next(true))
     );
-
-    ngOnInit() {
-        this.showTimesUpMsg$.subscribe();
-    }
-
 }
